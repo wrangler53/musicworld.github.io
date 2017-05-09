0b5e811b97e10a7c276b9030d04cd6dd48951e14
Yarik Melnichuk <melnichukjarik@gmail.com>
2017-05-10 00:00:07 +0300

    Adaptive design

---
+++ form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Server</title>
+	<style>
+		h1, .return {
+			text-align: center;
+			margin-top: 200px;
+		}
+		.return a {
+			color: black;
+			font-size: 24px;
+		}
+	</style>
+</head>
+<body>
+	<?php
+		echo "<h1>Data already has been sent!<br>Thank you!</h1>";
+	?>
+	<div class="return">
+		<a href="about.html">Return</a>
+	</div>
+</body>
+</html>
